1acf62236f33108aa68e9e5e40fff8218cc66ad7
Vikram Sharma <vsharma8363@gmail.com>
2018-02-12 19:45:44 -0800

    Fixed bug in drop and pickup

--- src/org/usfirst/frc/team670/robot/commands/actions/DropCube.java
+++ src/org/usfirst/frc/team670/robot/commands/actions/DropCube.java
@@ -31,7 +31,7 @@ public class DropCube extends Command {
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        if(Robot.sensors.getDistanceIntakeInches() <= 12 || com.isCompleted())
+        if(Robot.sensors.getDistanceIntakeInches() >= 6 || com.isCompleted())
         	return true;
         else
         	return false;

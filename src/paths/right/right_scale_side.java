7c47921d2c60daa3b8cc750444af8e0e1ccdacc9
Shaylan Dias <shaylandias@gmail.com>
2018-03-09 19:34:24 -0800

    Scale Side Path fix

--- src/paths/right/right_scale_side.java
+++ src/paths/right/right_scale_side.java
@@ -1,12 +1,11 @@
 package paths.right;
 
 import org.usfirst.frc.team670.robot.Robot;
-import org.usfirst.frc.team670.robot.commands.auto_specific.DropCube;
+import org.usfirst.frc.team670.robot.commands.DelayedRaise;
+import org.usfirst.frc.team670.robot.commands.auto_specific.ShootCube;
 import org.usfirst.frc.team670.robot.commands.drive.Drive;
 import org.usfirst.frc.team670.robot.commands.drive.Pivot;
 import org.usfirst.frc.team670.robot.commands.elevator.Encoders_Elevator;
-import org.usfirst.frc.team670.robot.commands.elevator.Hold_Elevator;
-import org.usfirst.frc.team670.robot.commands.intake.Deploy;
 import org.usfirst.frc.team670.robot.constants.Field;
 import org.usfirst.frc.team670.robot.constants.RoboConstants;
 import org.usfirst.frc.team670.robot.constants.enums.ElevatorState;
@@ -19,11 +18,12 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class right_scale_side extends CommandGroup {
 
 	public right_scale_side() {
-		addSequential((new Drive(Field.DS_TO_SCALE - 28.26/(Math.sin(Math.toRadians(45))) - Robot.length)));
+	addParallel(new DelayedRaise(ElevatorState.CLEARANCE));
+    	addSequential((new Drive(Field.DS_TO_SCALE - 28.26 - Robot.length)));
     	addParallel(new Encoders_Elevator(ElevatorState.HIGHSCALE));
     	addSequential(new Pivot(-45));
     	addSequential(new Drive(28.26/Math.sin(Math.toRadians(45)) + RoboConstants.FRONT_TO_ELEVATOR + Field.TOLERANCE/2));
-    	addSequential(new DropCube());
+    	addSequential(new ShootCube());
     	addParallel(new Encoders_Elevator(ElevatorState.SWITCH));
     	addSequential(new Drive(-(RoboConstants.FRONT_TO_ELEVATOR + 28.26/(Math.sin(Math.toRadians(45))))));
     	addSequential(new Pivot(-90));

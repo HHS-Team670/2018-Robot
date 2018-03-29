e982884a47d6192cc6cd019e14c9d5007184da49
Shaylan Dias <shaylandias@gmail.com>
2018-03-28 21:15:13 -0700

    Gets Both Encoder Velocities for Logging

--- src/org/usfirst/frc/team670/robot/commands/drive/Encoders_Drive.java
+++ src/org/usfirst/frc/team670/robot/commands/drive/Encoders_Drive.java
@@ -113,7 +113,7 @@ public class Encoders_Drive extends LoggingCommand {
 		    put("IsWithinLimit", isWithinLimit);
 		    put("FinalYaw", Robot.sensors.getYaw());
 		    put("LeftEncoderVelocity", leftEncoder.getQuadratureVelocity());
-		    put("RightEncoderVelocity", leftEncoder.getQuadratureVelocity());
+		    put("RightEncoderVelocity", rightEncoder.getQuadratureVelocity());
 		    put("LeftEncoderTicks", leftEncoder.getQuadraturePosition());
 		    put("RightEncoderTicks", rightEncoder.getQuadraturePosition());
 		}});

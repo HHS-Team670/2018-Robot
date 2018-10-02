1c9b319c88268def01ef64195db025903dfb9acc
ShaylanDias <sdias695@student.fuhsd.org>
2018-10-02 15:06:09 -0700

    Oops I never actually set the driver state

--- src/org/usfirst/frc/team670/robot/Robot.java
+++ src/org/usfirst/frc/team670/robot/Robot.java
@@ -431,6 +431,9 @@ public class Robot extends TimedRobot {
 
 	@Override
 	public void teleopInit() {
+		
+		driveBase.setDriveState(driverState.getSelected());
+		
 		if (combined != null) {
 			combined.cancel();
 		}
